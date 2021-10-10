f4ba3c45d439c68f8b97c59cdb0934d5678fb2b6
yutaro.aoyagi <independent.aobr@gmail.com>
2021-10-10 07:11:24 +0000

    toppage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: 'tasks#index'
+ root to: 'toppages#index'
+ 
+ get 'login', to: 'sessions#new'
+ post 'login', to: 'sessions#create'
+ delete 'logout', to: 'sessions#destroy'
+ 
+ get 'signup', to: 'users#new'
+ resources :users, only: [:index, :show, :create]
  
  resources :tasks
 end
