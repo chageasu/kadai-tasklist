3544c0d40fcc39501b210e03797100bbc6aa3f95
yutaro.aoyagi <independent.aobr@gmail.com>
2021-10-18 07:17:31 +0000

    lesson20kadai6

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,9 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   
   def index
-    @task = current_user.tasks.build
     @pagy,@tasks = pagy(current_user.tasks.order(id: :desc))
   end
 
